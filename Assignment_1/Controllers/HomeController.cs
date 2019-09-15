cf694a281a475100296a49bbcc6e3042f1b5d1dc
Sharath Kurian <sharathkottalil@gmail.com>
2019-09-16 06:14:18 +1000

    Changes
    Made a few changes

--- Assignment_1/Controllers/HomeController.cs
+++ Assignment_1/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace Assignment_1.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
