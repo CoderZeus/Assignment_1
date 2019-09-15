55d32e0f87721e684932c61d66ebe4d2b2a0479f
Sharath Kurian <sharathkottalil@gmail.com>
2019-09-16 03:41:03 +1000

    Add project files.
    w7 status

---
+++ Assignment_1/Controllers/NotificationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Assignment_1.Models.SendGrid;
+using System.Threading.Tasks;
+
+namespace Assignment_1.Controllers
+{
+    public class NotificationController : Controller
+    {
+        // GET: Notification
+        public ActionResult EmailNotify()
+        {
+            SendGridClass SendGridClass0bj =new SendGridClass();
+            //Task t = Task.Factory.StartNew(SendGridClass0bj.Execute());
+            SendGridClass0bj.Execute();
+           // Exe
+            //SendGridClass
+
+
+
+
+
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ Assignment_1/Models/Email.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+using System.ComponentModel.DataAnnotations; //added eextra
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+
+// using SendGrid's C# Library
+// https://github.com/sendgrid/sendgrid-csharp
+using SendGrid;
+using SendGrid.Helpers.Mail;
+//using System;
+using System.Threading.Tasks;
+
+
+namespace Assignment_1.Models
+{
+    public class Email
+    {
+        [DataType(DataType.EmailAddress), Display(Name = "To")]
+        [Required]
+        public string ToEmail { get; set; }
+        [Display(Name = "Body")]
+        [DataType(DataType.MultilineText)]
+        public string EMailBody { get; set; }
+        [Display(Name = "Subject")]
+        public string EmailSubject { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "CC")]
+        public string EmailCC { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "BCC")]
+        public string EmailBCC { get; set; }
+
+
+        public static Message SendMessage(GmailService service, String userId, Message email)
+        {
+            try
+            {
+                return service.Users.Messages.Send(email, userId).Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return null;
+        }
+
+    }    
+
+namespace SendGrid
+    {
+        class SendGridClass
+        {
+            public  async Task<int> Execute()
+            {
+                var apiKey = "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";
+                //var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress("test@example.com", "Example User");
+                var subject = "New Login from Device";
+                var to = new EmailAddress("sharathkottalil@gmail.com", "Example User");
+                var plainTextContent = "and easy to do anywhere, even with C#";
+                var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+
+                return 1;
+            }
+        }
+    }
+
+
+}
